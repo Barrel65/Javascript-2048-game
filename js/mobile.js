6119d64fe4fecfb288e633617ab70d52223b0559
Barrel65 <a.m.sapron@gmail.com>
2023-03-14 03:49:50 +0700

    v1.23

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,5 @@
 
-if (is_touch_device) {
+if (is_touch_device()) {
 	const gameBoard = document.getElementById("gameBoard");
 	gameBoard.style.padding = "3vmin";
 	gameBoard.style.setProperty('--cell-size', "22vmin");
